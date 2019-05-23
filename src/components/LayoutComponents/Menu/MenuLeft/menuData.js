cf4ade91058c15e24d161afbf1d8d946a19a32c9
satdevops88 <sat.devops88@gmail.com>
2019-05-23 13:57:36 +0700

    version 1.0

---
+++ src/components/LayoutComponents/Menu/MenuLeft/menuData.js
@@ -0,0 +1,14 @@
+export default [
+  {
+    title: 'Dashboard Alpha',
+    key: 'dashboardAlpha',
+    url: '/contact',
+    icon: 'icmn icmn-stack',
+  },
+  {
+    title: 'Empty Page',
+    key: 'empty',
+    url: '/empty',
+    icon: 'icmn icmn-books',
+  },
+]
